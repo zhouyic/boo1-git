1ab8990350fd0fd5acae0fb35430c5cb65c03031
zhouyicai <592519679@qq.com>
2020-10-25 10:56:06 +0800

    构建boot1

---
+++ src/main/java/com/zyc/Boot1Application.java
@@ -0,0 +1,13 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot1Application.class, args);
+    }
+
+}
